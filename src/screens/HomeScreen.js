d30a4d1b3dbc18114e2700cc3c2017aef8526ddc
manis-h <manishabc83@gmail.com>
2023-10-19 01:59:51 +0530

    swiper

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,38 +1,55 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import Card from '../components/TinderCard';
-import users from '../../assets/data/users';
+import { View, ImageBackground, StyleSheet, Button, Text } from 'react-native';
 
-import AnimatedStack from '../components/AnimatedStack';
+import Swiper from 'react-native-deck-swiper'
 
-const HomeScreen = () => {
-  const onSwipeLeft = user => {
-    console.warn('swipe left', user.name);
-  };
 
-  const onSwipeRight = user => {
-    console.warn('swipe right: ', user.name);
-  };
-
+const Home = () => {
   return (
-    <View style={styles.pageContainer}>
-      <AnimatedStack
-        data={users}
-        renderItem={({item}) => <Card user={item} />}
-        onSwipeLeft={onSwipeLeft}
-        onSwipeRight={onSwipeRight}
-      />
+    
+       <View style={styles.container}>
+        <Swiper
+            cards={[ 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+            renderCard={(card) => {
+                return (
+                    <View style={styles.card}>
+                        <Text style={styles.text}>{card}</Text>
+                    </View>
+                )
+            }}
+            onSwiped={(cardIndex) => {console.log(cardIndex)}}
+            onSwipedAll={() => {console.log('onSwipedAll')}}
+            cardIndex={2}
+            backgroundColor={'#4FD0E9'}
+            stackSize= {2}>
+            <Button
+                onPress={() => {console.log('oulala')}}
+                title="Press me">
+                You can press me
+            </Button>
+        </Swiper>
     </View>
+
+    
   );
 };
-
 const styles = StyleSheet.create({
-  pageContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+  container: {
     flex: 1,
-    width: '100%',
+    backgroundColor: "#F5FCFF"
   },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
 });
-
-export default HomeScreen;
+export default Home;
